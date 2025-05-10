a59267549f63fe200fbd32ce408620db617f8e47
Jaheda Sultana <jahedasultana47@gmail.com>
2025-05-10 11:00:00 +0600

    appError added

--- src/app/middlewares/globalErrorHandler.ts
+++ src/app/middlewares/globalErrorHandler.ts
@@ -2,18 +2,27 @@
 /* eslint-disable @typescript-eslint/no-explicit-any */
 import { NextFunction, Request, Response } from "express"
 import { envVars } from "../config/env"
-import { success } from "zod"
+import AppError from "../errorHelpers/AppError"
 
 
 export const globalErrorHandler = (err: any, req: Request, res: Response, next: NextFunction) => {
 
 
-const statusCode = 500
-const message = `Something Went Wrong!! ${err.message} from global error`
- res.status(statusCode).json({
-    success: false,
-    message,
-    err,
-    stack: envVars.NODE_ENV === "development" ? err.stack : null
- })
+   let statusCode = 500
+   let message = "Something Went Wrong!!"
+   
+    if (err instanceof AppError) {
+        statusCode = err.statusCode
+        message = err.message
+    } else if (err instanceof Error) {
+        statusCode = 500;
+        message = err.message
+    }
+
+   res.status(statusCode).json({
+      success: false,
+      message,
+      err,
+      stack: envVars.NODE_ENV === "development" ? err.stack : null
+   })
 }
